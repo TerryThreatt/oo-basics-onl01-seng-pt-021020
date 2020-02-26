85d3a617cd8e5650d14d036a2965d97d3651f005
Terry Threatt <terry.threatt@gmail.com>
2020-02-26 18:16:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,13 @@
 # Make your shoe class here!
 
 class Shoe 
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def cobble
+    puts ""
 end 
